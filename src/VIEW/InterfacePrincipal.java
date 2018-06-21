2b43ba0b487f12faf99204d9b4d4970159775e90
marcos <marcos@Vostro-3450>
2018-06-21 12:09:11 -0300

    Recebido as classes. Trabalhando em cima.

--- src/VIEW/InterfacePrincipal.java
+++ src/VIEW/InterfacePrincipal.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package VIEW;
 
-/**
- *
- * @author bruno.coaracy
- */
+
 public class InterfacePrincipal extends javax.swing.JFrame {
 
     /**
@@ -43,9 +35,7 @@ public class InterfacePrincipal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
