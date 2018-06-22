7c37b6a2a3473e4ddb0fce68f55e2bd3d53ddf34
bruno <bruno.coaracy@gmail.com>
2018-06-22 00:33:33 -0300

    Adicionado BEANALuno/DAOAluno alterado o Controller

--- src/CONTROLLER/Controller.java
+++ src/CONTROLLER/Controller.java
@@ -1,6 +1,15 @@
 package CONTROLLER;
 
+import MODEL.BEANAluno;
+import MODEL.DAOAluno;
 
 public class Controller {
-    
+    //    Alunos
+    public static BEANAluno buscaAluno(String nome) {
+        return DAOAluno.getInstance().find(nome);
+    }
+
+    public static void addUsuario(BEANAluno aluno) {
+        DAOAluno.getInstance().create(aluno);
+    }
 }
---
+++ src/MODEL/DAOAluno.java
@@ -0,0 +1,43 @@
+package MODEL;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DAOAluno {
+private static DAOAluno instance;
+
+    public DAOAluno() {
+        MySQLDAO.getConnection();
+    }
+
+    public static DAOAluno getInstance() {
+        if (instance == null) {
+            instance = new DAOAluno();
+        }
+        return instance;
+    }
+
+    public BEANAluno find(String nome) {
+        BEANAluno aluno = new BEANAluno();
+        String query = "SELECT * from <tabela_dos_alunos> WHERE <coluna do NOme> = '" + nome + "'";
+        ResultSet rs = MySQLDAO.getResultSet(query);
+        try {
+            if (rs.next()) {
+                aluno.setId(rs.getInt("id"));
+                aluno.setNome(rs.getString("nome"));
+            }
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOAluno.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return aluno;
+    }
+
+    public void create(BEANAluno aluno) {
+        String query = "INSERT INTO <tabela_dos_alunos> (`nome`) VALUES ('"  + aluno.getNome() + "')";
+        MySQLDAO.executeQuery(query);
+    }    
+}
