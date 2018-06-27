f31c98d9255992dd0f7d2dd9f337cb1d2b70c0e1
bruno <bruno.coaracy@gmail.com>
2018-06-27 18:29:43 -0300

    Alterado a tela cadastro aluno implementado o botão cadastrar Corrigido algumas paradas Controller

--- src/CONTROLLER/Controller.java
+++ src/CONTROLLER/Controller.java
@@ -3,12 +3,15 @@ package CONTROLLER;
 import MODEL.*;
 
 public class Controller {
-    //    Alunos
-    public static BEANPessoa buscaAluno(String nome) {
-        return DAOPessoa.getInstance().find(nome);
-    }
+//    Alunos
+//    public static BEANPessoa buscaAluno(String nome) {
+//        return DAOPessoa.getInstance().find(nome);
+//    }
 
     public static void criaAluno(BEANPessoa aluno) {
-        DAOPessoa.getInstance().create(aluno);
+//        DAOPessoa.getInstance().create(aluno);
     }
+    
+    
+    //
 }
